772282a9a0cc8c1e143babb98f478143567aa82b
Katie Egervari <katie.egervari@gmail.com>
2018-10-22 07:54:40 -0400

    Reducers now call create() function to set the default value

--- src/schematics/ngrx-reducer/files/__name@dasherize__.reducer.ts
+++ src/schematics/ngrx-reducer/files/__name@dasherize__.reducer.ts
@@ -1,21 +1,20 @@
 import { Action } from '@ngrx/store';
 
+import { create<%= classify(name) %> } from '../types/<%= dasherize(name) %>-state/<%= dasherize(name) %>-state.functions';
 import { <%= classify(name) %>State } from '../types/<%= dasherize(name) %>-state/<%= dasherize(name) %>-state.interface';
 
 import { <%= classify(name) %>Actions } from './<%= dasherize(name) %>.actions';
 
 export function <%= camelize(name) %>Reducer(
-  state: <%= classify(name) %>State,
+  state: <%= classify(name) %>State = create<%= classify(name) %>(),
   action: Action
 ): <%= classify(name) %>State {
   switch (action.type) {
     case <%= classify(name) %>Actions.RETRIEVE:
       return {
-        ...state,
-      };
-    default:
-      return {
         ...state
       };
+    default:
+      state;
   }
 }
