48479dcd8fd994d51fafc332d06016ad326ad59f
Katie Egervari <katie.egervari@gmail.com>
2018-10-22 14:35:38 -0400

    Simplify tslint rule

--- src/types/rules/run-tslint.ts
+++ src/types/rules/run-tslint.ts
@@ -3,15 +3,10 @@ import { TslintFixTask } from '@angular-devkit/schematics/tasks';
 
 export function runTslint(): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    const dir = tree.getDir('/');
-
-    if (dir && (dir.subfiles as string[]).includes('tslint.json')) {
-      context.addTask(
-        new TslintFixTask({
-          ignoreErrors: true,
-          tsConfigPath: 'tsconfig.json'
-        })
-      );
-    }
+    context.addTask(
+      new TslintFixTask({
+        ignoreErrors: true
+      })
+    );
   };
 }
