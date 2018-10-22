16c438ecf4837b0c0f37b83875c1c3c2eb2d4446
Katie Egervari <katie.egervari@gmail.com>
2018-10-22 14:36:37 -0400

    Removed tree

--- src/types/rules/run-tslint.ts
+++ src/types/rules/run-tslint.ts
@@ -1,8 +1,8 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext } from '@angular-devkit/schematics';
 import { TslintFixTask } from '@angular-devkit/schematics/tasks';
 
 export function runTslint(): Rule {
-  return (tree: Tree, context: SchematicContext) => {
+  return (_, context: SchematicContext) => {
     context.addTask(
       new TslintFixTask({
         ignoreErrors: true
