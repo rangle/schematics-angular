9290ecbff756a59ed12ad18c4587399ea1593d5b
Katie Egervari <katie.egervari@gmail.com>
2018-10-22 14:23:00 -0400

    Re-added imports for ng module

--- src/schematics/feature/files/__name@dasherize__.module.ts
+++ src/schematics/feature/files/__name@dasherize__.module.ts
@@ -1,3 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+
 import { <%= classify(name) %>Service } from './services/<%= dasherize(name) %>.service';
 import { <%= classify(name) %>Effects } from './store/<%= dasherize(name) %>.effects';
 import { <%= classify(name) %>Store } from './store/<%= dasherize(name) %>.store';
