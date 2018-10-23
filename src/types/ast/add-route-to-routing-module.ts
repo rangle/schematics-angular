6cc1690533e4bb2ddd3f29a17af8763acfe4598f
Katie Egervari <katie.egervari@gmail.com>
2018-10-23 15:25:25 -0400

    Cast as identifier

--- src/types/ast/add-route-to-routing-module.ts
+++ src/types/ast/add-route-to-routing-module.ts
@@ -28,9 +28,7 @@ export function addRouteToRoutingModule(
       'path'
     );
 
-    console.log(pathProperty.initializer);
-
-    return true;
+    return (pathProperty.initializer as typescript.Identifier).text === '**';
   });
 
   return [
