1d0f7306bc3fa049814f7bf13cf494d6fa736539
Katie Egervari <katie.egervari@gmail.com>
2018-10-23 09:02:54 -0400

    Adds EffectsModule to module if it does not exist in imports

--- src/schematics/ngrx-effects/index.ts
+++ src/schematics/ngrx-effects/index.ts
@@ -1,7 +1,7 @@
 import * as strings from '@angular-devkit/core/src/utils/strings';
 import { chain, Rule } from '@angular-devkit/schematics';
-import { addImportToModule } from '@schematics/angular/utility/ast-utils';
 
+import { addEffectsToModule } from '../../types/ast/add-effects-to-module';
 import { Folders } from '../../types/folders/folders.enum';
 import { processTemplates } from '../../types/path-options/path-options.functions';
 import { modifySourceFileRule } from '../../types/rules/modify-source-file.rule';
@@ -22,10 +22,10 @@ export default function(options: SchemaOptions): Rule {
     modifySourceFileRule(
       tree => findModuleFilenameInTree(tree, options),
       (sourceFile, moduleFilename) =>
-        addImportToModule(
+        addEffectsToModule(
           sourceFile,
           moduleFilename,
-          `EffectsModule.forFeature([${strings.classify(`${options.name}Effects`)}])`,
+          strings.classify(`${options.name}Effects`),
           `.${Folders.Store}/${strings.dasherize(options.name)}.effects`
         )
     )
---
+++ src/types/ast/add-effects-to-module.ts
@@ -0,0 +1,61 @@
+import { getDecoratorMetadata, insertImport } from '@schematics/angular/utility/ast-utils';
+import { Change, InsertChange } from '@schematics/angular/utility/change';
+import * as typescript from 'typescript';
+
+function getNgModuleNode(sourceFile: typescript.SourceFile): typescript.ObjectLiteralExpression {
+  const nodes = getDecoratorMetadata(sourceFile, 'NgModule', '@angular/core');
+
+  return nodes.length === 1 && nodes[0].kind === typescript.SyntaxKind.ObjectLiteralExpression
+    ? (nodes[0] as typescript.ObjectLiteralExpression)
+    : null;
+}
+
+function getNgModuleNodeProperty(
+  ngModuleNode: typescript.ObjectLiteralExpression,
+  propertyName: string
+): typescript.PropertyAssignment {
+  const properties = ngModuleNode.properties
+    .filter(property => property.kind === typescript.SyntaxKind.PropertyAssignment)
+    .filter(property => property.name.kind === typescript.SyntaxKind.Identifier)
+    .filter(property => (property.name as typescript.Identifier).text === propertyName);
+
+  return properties.length === 1 ? (properties[0] as typescript.PropertyAssignment) : null;
+}
+
+export function getPropertyAssignmentArrayElements(
+  propertyAssignment: typescript.PropertyAssignment
+): typescript.Node[] {
+  return ((propertyAssignment.initializer as typescript.ArrayLiteralExpression)
+    .elements as {}) as typescript.Node[];
+}
+
+export function addEffectsToModule(
+  sourceFile: typescript.SourceFile,
+  modulePath: string,
+  classifiedName: string,
+  importPath: string
+): Change[] {
+  const ngModuleNode = getNgModuleNode(sourceFile);
+
+  if (ngModuleNode) {
+    const propertyAssignment = getNgModuleNodeProperty(ngModuleNode, 'imports');
+
+    if (propertyAssignment) {
+      const elements = getPropertyAssignmentArrayElements(propertyAssignment);
+      if (elements.map(element => element.getText()).includes('EffectsModule')) {
+        console.log('has effects module');
+      } else {
+        return [
+          new InsertChange(
+            modulePath,
+            elements[elements.length - 1].getEnd(),
+            `EffectsModule.forFeature([${classifiedName}])`
+          ),
+          insertImport(sourceFile, modulePath, classifiedName, importPath)
+        ];
+      }
+    }
+  }
+
+  return [];
+}
