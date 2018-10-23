e2701a04ab35e66bca52bbe8bd7a7c5b4aecbc18
Katie Egervari <katie.egervari@gmail.com>
2018-10-23 13:47:06 -0400

    print children

--- src/types/ast/add-route-to-routing-module.ts
+++ src/types/ast/add-route-to-routing-module.ts
@@ -1,12 +1,20 @@
 import { Change } from '@schematics/angular/utility/change';
 import * as typescript from 'typescript';
 
+function printAllChildren(node: typescript.Node, depth = 0) {
+  console.log(new Array(depth + 1).join('--'), node.kind, node.pos, node.end);
+  depth++;
+  node.getChildren().forEach(c=> printAllChildren(c, depth));
+}
+
 export function addRouteToRoutingModule(
   sourceFile: typescript.SourceFile,
   modulePath: string,
   classifiedName: string,
   importPath: string
 ): Change[] {
+  printAllChildren(sourceFile);
+
   const variableStatement = sourceFile.statements.find(
     statement => statement.kind === typescript.SyntaxKind.VariableStatement
   );
@@ -15,7 +23,6 @@ export function addRouteToRoutingModule(
 
   declarations.forEach(
     declaration => {
-      console.log(declaration.type);
       console.log((declaration.type as typescript.TypeReferenceNode).typeName);
     }
   );
